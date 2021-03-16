f0c7dd90f03d6ecfe579aaac6546104c1188bba1
Pedro Marques Freire <pedromarques1011@gmail.com>
2021-03-16 10:28:16 -0300

    t

--- api/auth/validateJWT.js
+++ api/auth/validateJWT.js
@@ -1,36 +1,38 @@
-const jwt = require('jsonwebtoken');
-const model = require('../../models/user');
+const jwt = require("jsonwebtoken");
+const model = require("../../models/user");
 
-const segredo = 'seusecretdetoken';
+const segredo = "seusecretdetoken";
 
 module.exports = (requireAdmin) => {
   return async (req, res, next) => {
-    const token = req.headers['authorization'];
+    const token = req.headers["authorization"];
 
     if (!token) {
-      res.status(400).json({ error: 'Token não encontrado ou informado' });
+      res.status(400).json({ error: "Token não encontrado ou informado" });
     }
 
     try {
       const decoded = jwt.verify(token, segredo);
-      console.log(decoded)
+      console.log(decoded);
       if (requireAdmin && !decoded.data.isAdmin) {
         res
           .status(200)
-          .json(400, { error: 'Acesso negado. Você não tem permissão de admin'});
+          .json(400, {
+            error: "Acesso negado. Você não tem permissão de admin",
+          });
       }
 
       const user = await model.findUser(decoded.data.username);
 
       if (!user) {
-        res.status(401).json({ message: 'Erro ao procurar usuario do token.' });
+        res.status(401).json({ message: "Erro ao procurar usuario do token." });
       }
 
       req.user = user;
 
       next();
     } catch (err) {
-      return res.status(401).json({ message: 'Erro: Seu token é inválido' });
+      return res.status(401).json({ message: "Erro: Seu token é inválido" });
     }
   };
-}
+};
